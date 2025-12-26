0bfb515b6e008979dc575fc7b85ea185ee8e74bc
10SEBASG01 <jandryelpro.ec@gmail.com>
2025-12-25 21:35:00 -0500

    Primer Commit CV

---
+++ Perfil/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import (
+    DatosPersonales, ExperienciaLaboral, Reconocimientos, 
+    CursosRealizados, ProductosAcademicos, ProductosLaborales, VentaGarage
+)
+
+@admin.register(DatosPersonales)
+class DatosPersonalesAdmin(admin.ModelAdmin):
+    list_display = ('nombres', 'apellidos', 'numerocedula', 'email_contacto')
+    fields = ('idperfil', 'fotoperfil', 'nombres', 'apellidos', 'email_contacto', 'descripcionperfil', 
+              'numerocedula', 'nacionalidad', 'fechanacimiento', 'sexo', 'estadocivil', 
+              'direcciondomiciliaria', 'sitioweb', 'perfilactivo')
+
+@admin.register(ExperienciaLaboral)
+class ExperienciaLaboralAdmin(admin.ModelAdmin):
+    list_display = ('cargodesempenado', 'nombrempresa', 'fechainiciogestion', 'activarparaqueseveaenfront')
+    list_filter = ('activarparaqueseveaenfront', 'nombrempresa')
+
+@admin.register(Reconocimientos)
+class ReconocimientosAdmin(admin.ModelAdmin):
+    list_display = ('descripcionreconocimiento', 'tiporeconocimiento', 'entidadpatrocinadora')
+    list_filter = ('tiporeconocimiento',)
+
+@admin.register(CursosRealizados)
+class CursosRealizadosAdmin(admin.ModelAdmin):
+    list_display = ('nombrecurso', 'entidadpatrocinadora', 'totalhoras')
+
+@admin.register(ProductosAcademicos)
+class ProductosAcademicosAdmin(admin.ModelAdmin):
+    list_display = ('nombrerecurso', 'clasificador', 'activarparaqueseveaenfront')
+
+@admin.register(ProductosLaborales)
+class ProductosLaboralesAdmin(admin.ModelAdmin):
+    list_display = ('nombreproducto', 'fechaproducto', 'activarparaqueseveaenfront')
+
+@admin.register(VentaGarage)
+class VentaGarageAdmin(admin.ModelAdmin):
+    list_display = ('nombreproducto', 'valordelbien', 'estadoproducto', 'activarparaqueseveaenfront')
+    list_filter = ('estadoproducto', 'activarparaqueseveaenfront')
---
+++ ProyectoHojaDeVida/settings.py
@@ -0,0 +1,111 @@
+import os
+import dj_database_url # Necesario para la base de datos de Render
+from pathlib import Path
+
+# Directorio Base
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Seguridad
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ww9j7_=ac%06&-rvo27ci!8f)0^2)+o-m8@1+i^bxys)=%l0@2')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
+# Render Hostnames
+ALLOWED_HOSTS = ['*']
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# Aplicaciones
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'whitenoise.runserver_nostatic', # Manejo de CSS en producción
+    'django.contrib.staticfiles',
+    'storages', # Librería para Azure
+    'Perfil',
+]
+
+# Middlewares
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Debe ir después de security
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'ProyectoHojaDeVida.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], 
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'ProyectoHojaDeVida.wsgi.application'
+
+# BASE DE DATOS: Usa Postgres si existe DATABASE_URL, si no SQLite local
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
+
+# CONFIGURACIÓN DE ARCHIVOS ESTÁTICOS (CSS, JS)
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# CONFIGURACIÓN DE ALMACENAMIENTO (Azure para Media, Whitenoise para Static)
+AZURE_STORAGE_CONNECTION_STRING = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
+
+if AZURE_STORAGE_CONNECTION_STRING:
+    AZURE_CONTAINER = 'media'
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.azure_storage.AzureStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
+else:
+    # Si no hay Azure configurado, guardamos localmente
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
+
+# Idioma y Hora
+LANGUAGE_CODE = 'es-ec'
+TIME_ZONE = 'America/Guayaquil' 
+USE_I18N = True
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Permite cargar PDFs en iframes (Vital para tu visor)
+X_FRAME_OPTIONS = 'SAMEORIGIN'
